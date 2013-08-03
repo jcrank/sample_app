a12c72df971e258f8345d27779dfd4caf4f89260
jcrank <joshua.crank@gmail.com>
2013-08-03 14:02:10 -0500

    switching tasks on 8.1.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 SampleApp::Application.routes.draw do
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root :to => 'static_pages#home'
 
   match '/signup', to: 'users#new'
+
+  match '/signin', to: 'sessions#new'
+
+  match '/signout', to: 'sessions#destroy'
+
+
+
 
   match '/help', to: 'static_pages#help'
 
